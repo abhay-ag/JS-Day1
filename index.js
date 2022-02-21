28e0ee167686dda08055dfb4bf116ec05cd5cc46
abhay_ag <foxyabhay@gmail.com>
2022-02-21 15:36:41 +0530

    Second-Commit

--- index.js
+++ index.js
@@ -14,3 +14,22 @@ function save(){
     counter.textContent = '0'
 }
 )
+
+// BlackJack
+
+let firstCard = 10;
+let secondCard = 11;
+let sum = firstCard + secondCard
+let blackjack  = false
+let isAlive = true
+if (sum <= 20){
+    console.log("Do you want to draw a new Card")
+}
+else if(sum === 21){
+    console.log("Woohoo! You've got a blackjack")
+    blackjack = true
+}
+else{
+    console.log("You're out of the Game!")
+    isAlive = false
+}
